56729af3effd5332b8905b57af247ebe1a083fa6
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-13 15:21:23 +0100

    dd

--- app/components/Game/Game.tsx
+++ app/components/Game/Game.tsx
@@ -18,19 +18,26 @@ export const Game = () => {
   // const [incrementAmount, setIncrementAmount] = useState(2)
 
   return (
-    <div>
-      
-      <div>Score: {score}</div>
-      <CanvasBoard height={400} width={800} />
-      <div>Instructiones</div>
-      <div>Para es press "d" to start</div>
-      <div>Movement optiones:</div>
-      <div>Left: "a"</div>
-      <div>Right: "d"</div>
-      <div>Up: "w"</div>
-      <div>Down: "s"</div><br />
+    <div style={{display: "flex"}}>
 
-      <div className='text-mute muted cool-grey'>This game is incomplete, but enjoy :)</div>
+      <div>
+        <h3>Instructiones</h3>
+        <div>Para es press "d" to start</div><br />
+
+        <strong>Movement optiones:</strong>
+        <div>Left: "a"</div>
+        <div>Right: "d"</div>
+        <div>Up: "w"</div>
+        <div>Down: "s"</div><br />
+
+        <div className='text-mute muted cool-grey'>This game is incomplete, but enjoy :)</div>
+      </div>
+
+      <div>
+        <div>Score: {score}</div>
+        <CanvasBoard height={300} width={700} />
+      </div>
+
     </div>
   )
 }
